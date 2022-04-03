2978204edc9be3c7cfdc446647ddbf362d926451
sabbir10s <sabbirahmed1023@gmail.com>
2022-04-04 00:47:57 +0600

    all basic setup done

---
+++ src/Components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h3>Page not Available </h3>
+        </div>
+    );
+};
+
+export default NotFound;
---
+++ src/Components/Reviews/Reviews.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Reviews = () => {
+    return (
+        <div>
+            <h1>Review Section</h1>
+        </div>
+    );
+};
+
+export default Reviews;
