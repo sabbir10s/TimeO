aa8021e91d498913cf1bb74bc6628f0cc58db513
sabbir10s <sabbirahmed1023@gmail.com>
2022-05-31 00:47:50 +0600

    test

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -21,7 +21,7 @@ const Home = () => {
 
             <div className='mt-12 pt-8'>
 
-                <h2 className='text-center text-3xl font-bold  py-7'>Customers Reviews ({reviews.length})</h2>
+                <h2 className='text-center text-3xl font-bold  py-7'>Customers Reviews</h2>
 
                 <div className='grid grid-cols-1 md:grid-cols-3 gap-7 content-center mx-10'>
                     {
